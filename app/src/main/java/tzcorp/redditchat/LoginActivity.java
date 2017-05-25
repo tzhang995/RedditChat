f466d188703df6f12162854476f4f0aedaa06013
Tony Zhang <tzhang995@gmail.com>
2017-05-25 18:43:57 -0400

    Retrieving Reddit token for accounts working, still need to login with the token

--- app/src/main/java/tzcorp/redditchat/LoginActivity.java
+++ app/src/main/java/tzcorp/redditchat/LoginActivity.java
@@ -2,10 +2,12 @@ package tzcorp.redditchat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +26,19 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by tony on 09/05/17.
@@ -54,6 +69,62 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(getIntent()!=null && Intent.ACTION_VIEW.equals(getIntent().getAction())) {
+            Uri uri = getIntent().getData();
+            if(uri.getQueryParameter("error") != null) {
+                String error = uri.getQueryParameter("error");
+                Log.e(TAG, "An error has occurred : " + error);
+            } else {
+                String state = uri.getQueryParameter("state");
+                if(state.equals(STATE)) {
+                    String code = uri.getQueryParameter("code");
+                    getAccessToken(code);
+                }
+            }
+        }
+    }
+
+    private void getAccessToken(String code) {
+        OkHttpClient client = new OkHttpClient();
+        String authString = CLIENT_ID + ":";
+        String encodedAuthString = Base64.encodeToString(authString.getBytes(), Base64.NO_WRAP );
+
+        Request request = new Request.Builder()
+                .addHeader("User-Agent", "RedditChat")
+                .addHeader("Authorization", "Basic " + encodedAuthString)
+                .url(ACCESS_TOKEN_URL)
+                .post(RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"),
+                        "grant_type=authorization_code&code=" + code +
+                                "&redirect_uri=" + REDIRECT_URI))
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "ERROR: " + e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+
+                JSONObject data = null;
+                try {
+                    data = new JSONObject(json);
+                    String accessToken = data.optString("access_token");
+                    String refreshToken = data.optString("refresh_token");
+
+                    Log.d(TAG, "Access Token = " + accessToken);
+                    Log.d(TAG, "Refresh Token = " + refreshToken);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
@@ -89,6 +160,9 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "Reddit Sign In");
+                String url = String.format(AUTH_URL, CLIENT_ID, STATE, REDIRECT_URI);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
             }
         });
 
